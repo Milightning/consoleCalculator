7138bce7031550539ce468875a341ed4c5bbf9a3
Milightning <milightning.ua@gmail.com>
2018-03-08 22:51:03 +0200

    test

---
+++ new_calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NCalc;
+
+namespace new_calculator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Write math expression or \"quit\"");
+            while (true)
+            {
+                var mathString = Console.ReadLine();
+                if (mathString == "quit")
+                {
+                    break;
+                }
+
+                Expression exp = new Expression(mathString);
+                try
+                {
+                    var result = exp.Evaluate();
+
+                    Console.WriteLine(result);
+                }
+
+                catch
+                {
+                    Console.WriteLine("Invalid expression.");
+                }
+
+            }
+        }
+
+    }
+}
